cd4a81236c176904357a41d62b7bc5b85c6da3dc
Johann Rudloff <johann@sinyax.net>
2019-02-09 18:42:03 +0100

    Implement flags for lwc.

--- src/bin/lwc.rs
+++ src/bin/lwc.rs
@@ -1,5 +1,8 @@
 extern crate livetools;
+extern crate getopts;
 
+use getopts::Options;
+use std::env;
 use std::time::{Instant};
 use std::io::prelude::*;
 use std::io;
@@ -12,6 +15,23 @@ struct WordCount {
     lines: u64,
     words: u64,
     in_word: bool,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct WordCountConfig {
+    count_bytes: bool,
+    count_lines: bool,
+    count_words: bool,
+}
+
+impl WordCountConfig {
+    pub fn new() -> Self {
+        WordCountConfig {
+            count_bytes: false,
+            count_lines: false,
+            count_words: false,
+        }
+    }
 }
 
 fn is_posix_whitespace(c: u8) -> bool {
@@ -32,40 +52,58 @@ fn is_word_char(c: u8) -> bool {
     // !c.is_ascii_whitespace();
 }
 
-fn update_wordcount(s: &WordCount, buf: &[u8]) -> WordCount {
+fn update_wordcount_chunk(cfg: &WordCountConfig, s: &WordCount, buf: &[u8]) -> WordCount {
     let mut upd = *s;
     upd.bytes += buf.len() as u64;
-    buf.iter().for_each(|b|
-                        {
-                            if *b == b'\n' {
-                                upd.lines += 1;
-                            }
-                            if is_word_char(*b) {
-                                if !upd.in_word {
-                                    upd.in_word = true;
-                                    upd.words += 1;
+    if cfg.count_words || cfg.count_lines {
+        buf.iter().for_each(|b|
+                            {
+                                if cfg.count_lines {
+                                    if *b == b'\n' {
+                                        upd.lines += 1;
+                                    }
                                 }
-                            } else {
-                                upd.in_word = false;
+                                if cfg.count_words {
+                                    if is_word_char(*b) {
+                                        if !upd.in_word {
+                                            upd.in_word = true;
+                                            upd.words += 1;
+                                        }
+                                    } else {
+                                        upd.in_word = false;
+                                    }
+                                }
                             }
-                        }
-                       );
+                           );
+    }
     return upd;
 }
 
-fn format_wordcount(wc: &WordCount) -> String {
-    return format!("{:8} {:7} {:8}", wc.lines, wc.words, wc.bytes);
+fn format_wordcount(cfg: &WordCountConfig, wc: &WordCount) -> String {
+    let mut output = String::with_capacity(64);
+
+    if cfg.count_lines {
+        output.push_str(&format!(" {:7}", wc.lines));
+    }
+    if cfg.count_words {
+        output.push_str(&format!(" {:7}", wc.words));
+    }
+    if cfg.count_bytes {
+        output.push_str(&format!(" {:7}", wc.bytes));
+    }
+
+    return output;
 }
 
-fn output_wordcount(wc: &WordCount) {
-    let wc_str = format_wordcount(wc);
+fn output_wordcount(cfg: &WordCountConfig, wc: &WordCount) {
+    let wc_str = format_wordcount(cfg, wc);
     io::stdout().write(
         format!("{}{}", CLEAR_LINE, wc_str).as_bytes()
         ).expect("Write failed");
     io::stdout().flush().expect("Flush failed");
 }
 
-fn live_wordcount() {
+fn live_wordcount(cfg: &WordCountConfig) {
     let mut wc = WordCount {
         bytes: 0,
         lines: 0,
@@ -81,18 +119,65 @@ fn live_wordcount() {
         if read == 0 {
             break
         }
-        wc = update_wordcount(&wc, &buffer[0..read]);
+        wc = update_wordcount_chunk(cfg, &wc, &buffer[0..read]);
 
         if last_update.map(|i| i.elapsed().as_secs()).unwrap_or(1) >= 1 {
-            output_wordcount(&wc);
+            output_wordcount(cfg, &wc);
             last_update = Some(Instant::now());
         }
     }
-    output_wordcount(&wc);
+    output_wordcount(cfg, &wc);
     io::stdout().write(b"\n").expect("Output Error");
     io::stdout().flush().expect("Output Error");
 }
 
+fn print_usage(program: &String, opts: &Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    live_wordcount()
+    let args: Vec<String> = env::args().collect();
+    let program = &args[0];//.clone();
+
+    let mut cfg = WordCountConfig::new();
+    let mut noflags = true;
+    let mut opts = Options::new();
+    opts.optflag("c", "bytes", "Print count of bytes");
+    opts.optflag("l", "lines", "Print count of lines");
+    opts.optflag("w", "words", "Print count of words");
+    opts.optflag("h", "help", "Show usage");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => {
+            print_usage(program, &opts);
+            println!("\nError parsing arguments:\n{}", f.to_string());
+            return;
+        }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(program, &opts);
+        return;
+    }
+
+    if matches.opt_present("c") {
+        cfg.count_bytes = true;
+        noflags = false;
+    }
+    if matches.opt_present("l") {
+        cfg.count_lines = true;
+        noflags = false;
+    }
+    if matches.opt_present("w") {
+        cfg.count_words = true;
+        noflags = false;
+    }
+    if noflags {
+        cfg.count_bytes = true;
+        cfg.count_lines = true;
+        cfg.count_words = true;
+    }
+
+    live_wordcount(&cfg)
 }
